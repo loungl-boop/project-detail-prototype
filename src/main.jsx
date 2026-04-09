768d8cb776640fe8c7ee7a69a1d6334613fd9372
yxl <yangxiaolong@miao.cn>
2026-04-09 14:44:22 +0800

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import ServiceDetail from './ServiceDetail.jsx'
+import ContractDetail from './ContractDetail.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router basename="/project-detail-prototype">
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/service-detail" element={<ServiceDetail />} />
+        <Route path="/contract-detail" element={<ContractDetail />} />
+      </Routes>
     </Router>
   </React.StrictMode>,
 )
