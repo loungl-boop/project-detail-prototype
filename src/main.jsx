c608427d58f79e115aede5813f8264749ec18548
yxl <yangxiaolong@miao.cn>
2026-04-09 14:35:19 +0800

    Fix React Router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
-import ContractDetail from './ContractDetail.jsx'
-import ServiceDetail from './ServiceDetail.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/contract-detail" element={<ContractDetail />} />
-        <Route path="/service-detail" element={<ServiceDetail />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router basename="/project-detail-prototype">
+      <App />
+    </Router>
+  </React.StrictMode>,
 )
