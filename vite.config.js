4c4ee0d021aff2cc47f89ba46b526f19dace11e1
yxl <yangxiaolong@miao.cn>
2026-04-09 13:56:05 +0800

    Fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 暂时使用根路径，后续根据实际仓库名称修改
+  base: '/project-detail-prototype/',
 })
